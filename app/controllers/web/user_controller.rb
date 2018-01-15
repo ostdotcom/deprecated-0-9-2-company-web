2c691c2f185bb72cc94960d6aa7bd55d30f772ca
Akshay <akshay.raje@gmail.com>
2018-01-15 14:14:19 +0530

    Login routes setup

---
+++ app/controllers/web/user_controller.rb
@@ -0,0 +1,23 @@
+class Web::UserController < Web::BaseController
+
+  layout "web"
+
+  before_action :set_page_meta_info
+
+  def login
+
+  end
+
+  def logout
+
+  end
+
+  def sign_up
+
+  end
+
+  def dashboard
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
   scope '', controller: 'web/home' do
     get '/' => :index
   end
+
+  scope '', controller: 'web/user' do
+    get '/login' => :login
+    get '/logout' => :logout
+    get '/sign-up' => :sign_up
+    get '/dashboard' => :dashboard_home
+  end
+
   # Route not found handler. Should be the last entry here
   match '*permalink', to: 'application#not_found', via: :all
 
