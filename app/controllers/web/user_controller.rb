d8720b81d3b0874a60eb1ea77c6a82ecde2e4541
Akshay <akshay.raje@gmail.com>
2018-02-05 14:22:27 +0530

    Planner

--- app/controllers/web/user_controller.rb
+++ app/controllers/web/user_controller.rb
@@ -5,7 +5,7 @@ class Web::UserController < Web::BaseController
   before_action :set_page_meta_info
 
   def login
-
+    @fe_no_nav = true;
   end
 
   def logout
@@ -17,6 +17,10 @@ class Web::UserController < Web::BaseController
   end
 
   def dashboard
+
+  end
+
+  def planner
 
   end
 
