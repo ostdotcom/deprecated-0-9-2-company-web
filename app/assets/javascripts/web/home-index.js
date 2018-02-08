edb6b4dbb29010120953c85d8bb276085ae17283
rachin <rachin@pepo.com>
2018-02-08 19:38:32 +0530

    make it work

--- app/assets/javascripts/web/home-index.js
+++ app/assets/javascripts/web/home-index.js
@@ -4,3 +4,4 @@
 //= require react_common
 //= require plugins/react-virtualized/react-virtualized
 //= require components/dashboard/transactions/transaction_list.es6.jsx
+//= require web/dashboardUtils
