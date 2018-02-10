21858a250c1d576d11a6b188741f46e26f17191b
Puneet Khushwani <puneet.khushwani@pepo.com>
2018-02-10 14:39:02 +0530

    fix

--- lib/company_api/response/entity/client_balances.rb
+++ lib/company_api/response/entity/client_balances.rb
@@ -23,11 +23,11 @@ module CompanyApi
         end
 
         def balances
-          @data['balances']
+          @data.present? ? @data['balances'] : {}
         end
 
         def conversion_rates
-          @data['conversion_rates']
+          @data.present? ? @data['conversion_rates'] : {}
         end
 
         def ost_wei_balance
@@ -35,13 +35,13 @@ module CompanyApi
         end
 
         def ost_balance
-          @o_b ||= ||= begin
+          @o_b ||= begin
             ost_wei_balance.present? ? convert_from_wei(ost_wei_balance) : nil
           end
         end
 
         def ost_fiat_balance(currency_pref)
-          ost_balance.present? convert_ost_to_fiat(ost_balance, currency_pref) : nil
+          ost_balance.present? ? convert_ost_to_fiat(ost_balance, currency_pref) : nil
         end
 
         def ost_prime_wei_balance
@@ -49,13 +49,13 @@ module CompanyApi
         end
 
         def ost_prime_balance
-          @o_p_b ||= ||= begin
+          @o_p_b ||= begin
             ost_prime_wei_balance.present? ? convert_from_wei(ost_prime_wei_balance) : nil
           end
         end
 
         def ost_prime_fiat_balance(currency_pref)
-          ost_prime_balance.present? convert_ost_to_fiat(ost_prime_balance, currency_pref) : nil
+          ost_prime_balance.present? ? convert_ost_to_fiat(ost_prime_balance, currency_pref) : nil
         end
 
         def bt_wei_balance
@@ -63,13 +63,13 @@ module CompanyApi
         end
 
         def bt_balance
-          @bt_b ||= ||= begin
+          @bt_b ||= begin
             bt_wei_balance.present? ? convert_from_wei(bt_wei_balance) : nil
           end
         end
 
         def bt_fiat_balance(currency_pref)
-          bt_balance.present? convert_bt_to_fiat(bt_balance, currency_pref) : nil
+          bt_balance.present? ? convert_bt_to_fiat(bt_balance, currency_pref) : nil
         end
 
         private
