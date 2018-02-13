30c842d04002e4b2c70d78a17472f72fb6de5a54
thahir-pepo <thahir@pepo.com>
2018-02-13 23:30:08 +0530

    help video js

--- app/assets/javascripts/web/economy/planner.js
+++ app/assets/javascripts/web/economy/planner.js
@@ -30,23 +30,6 @@
         }
       };
 
-      var $videoSrc;
-      $('.video-btn').click(function() {
-        $videoSrc = $(this).data( "src" );
-      });
-      console.log($videoSrc);
-      // when the modal is opened autoplay it
-      $('#helpVideoModal').on('shown.bs.modal', function (e) {
-
-      // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
-        $("#video").attr('src',$videoSrc + "?rel=0&amp;showinfo=0&amp;modestbranding=1&amp;autoplay=1" );
-      })
-      // stop playing the youtube video when I close the modal
-      $('#helpVideoModal').on('hide.bs.modal', function (e) {
-        // a poor man's stop video
-        $("#video").attr('src',$videoSrc);
-      });
-
     },
 
     grant_initial_ost: true, /* Over-Ride using config. */
