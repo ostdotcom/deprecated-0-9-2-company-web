36521321d521a41f128c06f1da2286b5f714b220
Akshay <akshay.raje@gmail.com>
2018-02-13 21:14:57 +0530

    Re-adding sign-up.js

---
+++ app/assets/javascripts/web/user/sign-up.js
@@ -0,0 +1,27 @@
+;
+(function (window) {
+
+  var parentNS = ns("simpletoken.home")
+    , utilsNs = ns("simpletoken.utils")
+    , oThis
+    ;
+
+  parentNS.signup = oThis = {
+    jForm: null
+    , init: function (config) {
+      oThis.bindEventListeners();
+    }
+    , bindEventListeners: function () {
+
+    }
+
+
+
+
+  };
+
+  $(document).ready(function () {
+    oThis.init({i18n: {}});
+  });
+
+})(window);
