7becdd317eae690fb44dc4f2a4b764c17c0f5ee6
Akshay <akshay.raje@gmail.com>
2018-02-19 14:19:46 +0530

    GoogleCharts

--- app/assets/javascripts/web/dashboard/GoogleCharts.js
+++ app/assets/javascripts/web/dashboard/GoogleCharts.js
@@ -4,9 +4,7 @@
     var oThis = this;
     $.extend( oThis, config );
 
-    if(!oThis.validate()){
-      return false;
-    }
+    oThis.load();
 
   };
 
@@ -19,21 +17,59 @@
     ajax: {},
     options: {},
     elem: null,
+    type: null,
 
     validate: function(){
       var oThis = this;
-      if (typeof google != "undefined" && typeof google.charts != "undefined" && typeof google.visualization != "undefined") {
-        google.charts.load(oThis.version, {packages: oThis.packages});
-      } else {
-        console.warn('Google charts not loaded. Please include https://www.gstatic.com/charts/loader.js');
+
+      if ( ($.isEmptyObject(oThis.data) && $.isEmptyObject(oThis.ajax)) || $.isEmptyObject(oThis.options) || !oThis.elem || !oThis.type ){
+        console.warn('Mandatory inputs for Google charts are missing [data,ajax,options,elem,type]');
         return false;
       }
 
-      if ( ($.isEmptyObject(oThis.data) && $.isEmptyObject(oThis.ajax)) || $.isEmptyObject(oThis.options) || !oThis.elem ){
-        console.warn('Mandatory inputs for Google charts are missing [data,ajax,options,elem]');
+      return true;
+    },
+
+    load: function(){
+      var oThis = this;
+      if (typeof google != "undefined" && typeof google.charts != "undefined") {
+        google.charts.load(oThis.version, {packages: oThis.packages});
+        console.log('Google charts loaded and ready to draw...');
+        oThis.draw();
+      } else {
+        console.warn('Google charts not loaded. Attempting to load https://www.gstatic.com/charts/loader.js asynchronously...');
+        $.ajax({
+          url: '//www.gstatic.com/charts/loader.js',
+          dataType: "script",
+          cache: true,
+          success: function(){
+            oThis.load();
+          },
+          failure: function(){
+            console.warn('Google charts not loaded. Could not load https://www.gstatic.com/charts/loader.js asynchronously');
+          }
+        });
+      }
+    },
+
+    draw: function(config){
+      var oThis = this;
+      $.extend( oThis, config );
+
+      console.log('draw called...');
+      console.log(google);
+
+      if ( ($.isEmptyObject(oThis.data) && $.isEmptyObject(oThis.ajax)) || $.isEmptyObject(oThis.options) || !oThis.elem || !oThis.type ){
+        console.warn('Mandatory inputs for Google charts are missing [data,ajax,options,elem,type]');
         return false;
       }
-      return true;
+
+      google.charts.setOnLoadCallback(function(){
+        var data = google.visualization.arrayToDataTable(oThis.data);
+        var chart = new google.visualization[oThis.type](document.getElementById(oThis.elem));
+        chart.draw(data, oThis.options);
+      });
+
     }
 
   };
--- app/assets/javascripts/web/economy/dashboard.js
+++ app/assets/javascripts/web/economy/dashboard.js
@@ -12,6 +12,22 @@
     ,init: function ( config ) {
       var oThis = this;
       oThis.simpleDataTable = new ost.SimpleDataTable();
+      oThis.googleCharts = new GoogleCharts({
+        data: [
+          ['Year', 'Sales', 'Expenses'],
+          ['2004',  1000,      400],
+          ['2005',  1170,      460],
+          ['2006',  660,       1120],
+          ['2007',  1030,      540]
+        ],
+        options: {
+          title: 'Company Performance',
+          curveType: 'function',
+          legend: { position: 'bottom' }
+        },
+        elem: 'transactionsValue',
+        type: 'LineChart'
+      });
     }
   };
 
