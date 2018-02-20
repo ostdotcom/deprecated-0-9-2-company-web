5d5eac92ba22a13adce3c9748f28dea2e1b9de4f
thahir-pepo <thahir@pepo.com>
2018-02-20 17:32:13 +0530

    ost home page

---
+++ app/assets/javascripts/web/ost/Index.js
@@ -0,0 +1,19 @@
+;
+(function (window) {
+
+  var oSTNs = ns("ost"),
+      oThis;
+
+  oSTNs.index = oThis = {
+
+    init: function (config) {
+
+    }
+
+  };
+
+  $(document).ready(function () {
+    oThis.init({i18n: {}});
+  });
+
+})(window);
