f81b96c7cdccc2d933ddb31ea6c41db0dbc16421
Ashutosh <ashutosh@ost.com>
2018-03-06 03:26:42 +0530

    bug fix

--- app/assets/javascripts/web/economy/tokenSupply.js
+++ app/assets/javascripts/web/economy/tokenSupply.js
@@ -33,7 +33,31 @@
       oThis.jShowWidgetBtn.off("click.tokenSupply").on("click.tokenSupply", function () {
         oThis.showMintWidget.apply(oThis, arguments);
       });
+
+      ost.mintBtWidget.jBtToMint.on("change" , function () {
+        oThis.updateMintTokensBtn();
+      });
+
+      ost.mintBtWidget.jStPrimeToMint.on("change" , function () {
+        oThis.updateMintTokensBtn();
+      });
+
     }
+
+    ,updateMintTokensBtn : function () {
+      var btToMint    = ost.mintBtWidget.jBtToMint.val() || 0 ,
+        stPrimeToMint = ost.mintBtWidget.jStPrimeToMint.val() || 0,
+        totalMint     = BigNumber(btToMint).plus( stPrimeToMint ),
+        jMintTokenBtn =  $('#mint-token-btn')
+      ;
+      console.log( "totalMint" , totalMint );
+      if( totalMint.isZero() ){
+        jMintTokenBtn.prop('disabled', true);
+      }else {
+        jMintTokenBtn.prop('disabled', false);
+      }
+    }
+
 
     , showMintWidget: function () {
       var oThis = this;
