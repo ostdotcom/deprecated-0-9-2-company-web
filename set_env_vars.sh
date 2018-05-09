3c5cde52b1aaaf699280ad92a31a78f33f9d51e6
Puneet Khushwani <puneet.khushwani@pepo.com>
2018-05-09 16:56:11 +0530

    changed dev env Secret KEy to keep in sync with stafing

--- set_env_vars.sh
+++ set_env_vars.sh
@@ -1,6 +1,6 @@
 # Note this secret has to be same for company-web & company-api in order to make CSRF work
 # As FE uses staging ENV for local testing, this should be in sync with Staging API Secret
-export COMPANY_SECRET_KEY_BASE='b1f0ff90cd692556f9740a8e609f88f2f4fc15d9dda9035445a7577c3f94936eaae91a0793c4ad5500314fe5a526a3b3f7c7c71c303f883d903df138783a8225'
+export COMPANY_SECRET_KEY_BASE='2fc2e8b316ceeb04e8675900f3176668d2a836494417e0164a966aefd701430390d3db685f87c224cbb48b332f8a5cf490fa7f034e063ff13a94fc06a409bf0a'
 
 # Core ENV Details
 export CW_SUB_ENV='sandbox'
