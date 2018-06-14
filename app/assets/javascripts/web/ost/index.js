1ecd1367754fa30382b6e6850c2778028f433f06
mayurpatil888 <mayur@ost.com>
2018-06-14 13:38:37 +0530

    confirmation checkbox on subscribe

--- app/assets/javascripts/web/ost/index.js
+++ app/assets/javascripts/web/ost/index.js
@@ -33,14 +33,30 @@
       var jsonpurl = $("#subscribe-form-submit").data('jsonp');
       var email = $("#subscribe-form-email").val();
 
-      var errors = [];
+      var errorLen = 0,
+      pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 
       if (email == '') {
-        errors.push('Email is Mandatory!');
+        oThis.showError('Email is Mandatory!', '.error' );
+        errorLen++;
+      } else if (pattern.test(email) == false) {
+        oThis.showError('Invalid email', '.error' );
+        errorLen++;
+      } else {
+        $('#subscribe-form .error').html('');
+      } 
+
+      if (! $("input#subscribe_confirmation").is(":checked")){
+
+        oThis.showError('please confirm checkbox', '.confirmation_error' );
+        errorLen++;
+
+      }else{
+        $('#subscribe-form .confirmation_error').html('');
       }
 
-      if (errors.length > 0) {
-        oThis.showError(errors.join(' '));
+      if (errorLen > 0) {
         return false;
       }
 
@@ -64,7 +80,7 @@
               });
             });
 
-            oThis.showError(error_msg.join('. '));
+            oThis.showError(error_msg.join('. '), '.general_error');
 
           } else {
 
@@ -74,8 +90,8 @@
           }
 
         },
-        error: function (response) {
-          oThis.showError('Something Went Wrong');
+        error: function (response, selector) {
+          oThis.showError('Something Went Wrong', '.general_error');
         },
         complete: function (response) {
           $("#subscribe-form-submit").prop('disabled', false);
@@ -86,13 +102,15 @@
 
 
 
-    showError: function (text) {
-      $('#subscribe-form .error').html(text);
+    showError: function (text, selector) {
+      $('#subscribe-form ' + selector).html(text);
       $('#subscribe-form-email').addClass('red');
     },
 
     resetError: function () {
-      $('#subscribe-form .error').html('');
+      $('#subscribe-form .confirmation_error, #subscribe-form .error, #subscribe-form .general_error').html('');
+      //$('#subscribe-form .error').html('');
+      //$('#subscribe-form .general_error').html('');
       $('#subscribe-form-email').removeClass('red');
     }
 
