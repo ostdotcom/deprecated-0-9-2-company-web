d7ab34aea90b5be2619bcb1220c63d2525d3c625
Shraddha <shraddha.patil@ost.com>
2018-08-16 22:05:45 +0530

    User list ke changes ho gaye.

--- app/assets/javascripts/web/economy/users/users-list.js
+++ app/assets/javascripts/web/economy/users/users-list.js
@@ -7,48 +7,44 @@
     ;
 
     var oThis = users.list = {
-        simpleDataTable : null,
-        init : function ( config ) {
+      simpleDataTable : null,
+      init : function ( config ) {
 
-          $('#show_user_list').hide()
-          $('#show_empty_user').hide()
+        $('#show_empty_user').hide();
+        $("#user_right_button_wrap").hide();
 
-            var oThis =  this,
-                isAirDropInProcess
-            ;
-            oThis.handelBarHelpers();
-            oThis.simpleDataTable =   new ost.SimpleDataTable({
-                jParent : $("#user_list"),
-                params  : {order_by: "created"}
-                //, resultFetcherCallback : show_user_list
-              //resultFetcherCallback : function(results){console.log(results)}
-            });
-
-            var result = oThis.simpleDataTable.getResults();
-            console.log(result)
-            if(result.length > 0) {
-              $('#show_user_list').show()
-              $('#show_empty_user').hide()
-            } else {
-              $('#show_user_list').hide()
-              $('#show_empty_user').show()
+          var oThis =  this,
+              isAirDropInProcess
+          ;
+          oThis.handelBarHelpers();
+          oThis.simpleDataTable =   new ost.SimpleDataTable({
+            jParent : $("#user_list")
+            , params  : {order_by: "created"}
+            , resultFetcherCallback : function ( results ) {
+              if ( results && results.length < 0 ) {
+                $('#show_user_list').show();
+                $('#show_empty_user').hide();
+              } else {
+                $('#show_user_list').hide();
+                $('#show_empty_user').show();
+              }
+              $("#user_right_button_wrap").show();
+              oThis.simpleDataTable.resultFetcherCallback = null;
             }
+          });
 
 
-            oThis.bindEvents();
-            isAirDropInProcess = $('.users-list-container').data('airdrop-processing');
-            if(!!isAirDropInProcess){
-              $('#airdrop_token_modal').modal('show');
-            }
+          oThis.bindEvents();
+          isAirDropInProcess = $('.users-list-container').data('airdrop-processing');
+          if(!!isAirDropInProcess){
+            $('#airdrop_token_modal').modal('show');
+          }
 
 
-            // $('#show_user_list').hide()
-            // $('#show_empty_user').hide()
-        }
-
-
-      ,
-        bindEvents : function () {
+          // $('#show_user_list').hide()
+          // $('#show_empty_user').hide()
+      }
+      , bindEvents : function () {
             var oThis =  this ,
                 config =  { success :  function () {
                                             var jForm = this.jForm;
@@ -89,59 +85,58 @@
             $('#airdrop_btn').on('click' ,  function () {
                 oThis.showAirDropTokenEditor();
             });
-        },
+      },
 
-       autoFocus : function() {
-         var jUserNameInput =  $('.add-user-name-input');
-         if( jUserNameInput && jUserNameInput.length > 0 ) {
-           jUserNameInput.eq(0).focus();
-         }
-       },
-
-        onFormSuccess : function ( response ) {
-            var oThis =  this;
-            oThis.updateNewAddedUser(response);
-        },
-
-        updateNewAddedUser : function ( response ) {
-            if( response && response.success ) {
-                var oThis       =  this
-                  , data        = response['data'] || {}
-                  , newData     = data["user"]
-                  , currentData
-                ;
-
-                currentData = oThis.simpleDataTable.getResultById( savedUserId );
-                console.log("currentData", currentData);
-                console.log("newData", newData);
-
-                $.extend(currentData , newData);
-                console.log("finalData", currentData);
-                oThis.updateUserMode(currentData);
-                oThis.simpleDataTable.updateResult( currentData );
-            }
-        },
-
-        updateUserMode : function (currentData) {
-            delete currentData['mode'];
-        },
-
-        showAirDropTokenEditor : function () {
-            ost.users.airDropEditor.showEditor();
-        },
-
-        handelBarHelpers : function () {
-            var oThis =  this;
-
-            Handlebars.registerHelper('ifEditUser', function( mode  , options ) {
-              if( mode === 'new' || mode === 'edit' ){
-                  return options.fn(this);
-              }else {
-                  return options.inverse(this);
-              }
-            });
+      autoFocus : function() {
+        var jUserNameInput =  $('.add-user-name-input');
+        if( jUserNameInput && jUserNameInput.length > 0 ) {
+         jUserNameInput.eq(0).focus();
         }
+      },
 
+      onFormSuccess : function ( response ) {
+          var oThis =  this;
+          oThis.updateNewAddedUser(response);
+      },
+
+      updateNewAddedUser : function ( response ) {
+          if( response && response.success ) {
+              var oThis       =  this
+                , data        = response['data'] || {}
+                , newData     = data["user"]
+                , currentData
+              ;
+
+              currentData = oThis.simpleDataTable.getResultById( savedUserId );
+              console.log("currentData", currentData);
+              console.log("newData", newData);
+
+              $.extend(currentData , newData);
+              console.log("finalData", currentData);
+              oThis.updateUserMode(currentData);
+              oThis.simpleDataTable.updateResult( currentData );
+          }
+      },
+
+      updateUserMode : function (currentData) {
+          delete currentData['mode'];
+      },
+
+      showAirDropTokenEditor : function () {
+          ost.users.airDropEditor.showEditor();
+      }
+
+      , handelBarHelpers : function () {
+          var oThis =  this;
+
+          Handlebars.registerHelper('ifEditUser', function( mode  , options ) {
+            if( mode === 'new' || mode === 'edit' ){
+                return options.fn(this);
+            }else {
+                return options.inverse(this);
+            }
+          });
+      }
     };
 
     function createNewUser() {
