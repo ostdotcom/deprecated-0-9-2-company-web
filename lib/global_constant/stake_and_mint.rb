d6b1dcf5dfa5185775f056ff89916f8ee465d24d
Puneet Khushwani <puneet.khushwani@ost.com>
2018-09-05 12:47:26 +0530

    added a hard cap on min ST Prime to mint value

---
+++ lib/global_constant/stake_and_mint.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+module GlobalConstant
+
+  class StakeAndMint
+
+    class << self
+
+      def max_value_of_min_stp_to_mint
+        5
+      end
+
+    end
+
+  end
+
+end
+
