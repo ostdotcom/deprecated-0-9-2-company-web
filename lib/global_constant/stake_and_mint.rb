00d841dfda3535411774db72d828af71ba62d4ae
AnaghaMurtarkar <anagha@ost.com>
2018-09-06 19:13:28 +0530

    changed value of max_value_of_min_stp_to_mint for testnet and mainnet

--- lib/global_constant/stake_and_mint.rb
+++ lib/global_constant/stake_and_mint.rb
@@ -6,7 +6,7 @@ module GlobalConstant
     class << self
 
       def max_value_of_min_stp_to_mint
-        5
+        GlobalConstant::StakeAndMint.main_sub_environment? ? 5 : 100
       end
 
     end
