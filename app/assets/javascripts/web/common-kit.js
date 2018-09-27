01ea100b01e90b3d7574893903e8a5b111ca6f92
Shraddha <shraddha.patil@ost.com>
2018-09-27 17:18:16 +0530

    3.3.1 juery to 2.2.4 + minor changes

--- app/assets/javascripts/web/common-kit.js
+++ app/assets/javascripts/web/common-kit.js
@@ -1,4 +1,4 @@
-//= require plugins/jquery/jquery-3.3.1
+//= require plugins/jquery/jquery-2.2.4
 //= require plugins/jquery-touchSwipe/jquery.touchSwipe
 //= require plugins/popper/popper
 //= require plugins/bootstrap/bootstrap
