d2e9fbf0afd5c74ab34a304d65307793b62f4f27
Shraddha <shraddha.patil@ost.com>
2018-12-11 17:28:27 +0530

    recaptcha added to signup page and other minor changes

--- app/assets/javascripts/web/user/sign-up.js
+++ app/assets/javascripts/web/user/sign-up.js
@@ -1,26 +1,39 @@
 ;
 (function (window) {
 
-  var parentNS = ns("simpletoken.home")
-    , utilsNs = ns("simpletoken.utils")
-    , oThis
-    ;
+  var parentNS = ns("ost.user")
+    , ost = ns("ost")
+  ;
 
   parentNS.signup = oThis = {
     jForm: null
     , init: function (config) {
+      oThis.jForm = $('#sign_up_form');
       oThis.bindEventListeners();
     }
     , bindEventListeners: function () {
-      var oThis = this;
-
-      var oldTimeOut;
+      // user token to be removed start
       $("#user-token-symbol").keyup( function () {
         clearTimeout( oldTimeOut );
         var newSymbol = this.value;
         oldTimeOut = setTimeout( function () {
           oThis.updateSymbols( newSymbol );
         }, 200);
+      });
+      //user token to be removed end
+
+      oThis.jForm.on('beforeSubmit',function (event) {
+        if(!oThis.isCaptchaValid){
+          event.preventDefault();
+        }
+      });
+
+      $('#register-btn').on('click',function () {
+        oThis.isCaptchaValid = ost.utilities.validateCaptcha(
+          oThis.jForm,
+          $('.recaptcha-submit-error'),
+          "Please select the captcha"
+        );
       });
     }
     , updateSymbols: function ( text ) {
