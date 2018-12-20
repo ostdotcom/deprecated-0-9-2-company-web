76b135f4223264e6a59a0339f8b12ac5b633a6ef
Preshita Shirke <preshita@ost.com>
2018-12-20 20:09:39 +0530

    modals for actions team page

--- app/assets/javascripts/web/user_setting/team.js
+++ app/assets/javascripts/web/user_setting/team.js
@@ -7,12 +7,14 @@
     simpleDataTable: null,
     jParent : $('#team_list'),
     jForm: $('#invite_new_member_form'),
+    sSelectPicker: 'select.selectpicker',
 
     init : function () {
       var datatableConfig={
         jParent:oThis.jParent,
         resultFetcherCallback : function ( results ) {
           oThis.initSelectPicker();
+          oThis.bindSelectPickerEvents();
         }
       };
       oThis.simpleDataTable = new ost.SimpleDataTable( datatableConfig );
@@ -26,11 +28,20 @@
       $("#invite_new_member_cancel_btn").on("click", function () {
         ost.coverElements.hide( $('#invite_new_member') );
       });
+      $(".delete-user").on("click", function () {
+        oThis.deleteUser();
+      });
+      $(".unassign-admin").on("click", function () {
+        oThis.unassignAdmin();
+      });
+      $(".assign-admin").on("click", function () {
+        oThis.assignAdmin();
+      });
 
     },
   
     initSelectPicker : function () {
-      $('select.selectpicker').selectpicker();
+      $(oThis.sSelectPicker).selectpicker();
     },
 
     initForm:function(){
@@ -44,7 +55,48 @@
 
     onInviteUserSuccess: function (  ) {
       ost.coverElements.hide( $('#invite_new_member') );
+    },
+
+    bindSelectPickerEvents:function() {
+      $(oThis.sSelectPicker).on('changed.bs.select', function (e, clickedIndex) {
+        switch( clickedIndex ) {
+          case 1:
+            oThis.resetSelectPicker();
+            $('#deleteUserModal').modal('show');
+            break;
+          case 2:
+            oThis.resetSelectPicker();
+            $('#unassignAdminModal').modal('show');
+            break;
+          case 3:
+            oThis.resetSelectPicker();
+            $('#assignAdminModal').modal('show');
+            break;
+        }
+      });
+    },
+
+    resetSelectPicker: function() {
+      $(oThis.sSelectPicker).val('default');
+      $(oThis.sSelectPicker).selectpicker("refresh");
+    },
+
+    deleteUser: function() {
+      $('#deleteUserModal').modal('hide');
+      $('#successModal').modal('show');
+    },
+
+    unassignAdmin: function() {
+      $('#unassignAdminModal').modal('hide');
+      $('#successModal').modal('show');
+    },
+
+    assignAdmin: function() {
+      $('#assignAdminModal').modal('hide');
+      $('#successModal').modal('show');
     }
+
+
   }
 
 })(window, jQuery);
