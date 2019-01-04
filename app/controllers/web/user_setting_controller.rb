6b3d7e8202fbaefc7448eef3d9945a19e16bce31
santhu210 <santhosh.nitdgp2015@gmail.com>
2019-01-04 16:28:49 +0530

    Changes for fetching client manager details on team page

--- app/controllers/web/user_setting_controller.rb
+++ app/controllers/web/user_setting_controller.rb
@@ -8,7 +8,20 @@ class Web::UserSettingController < Web::BaseController
   after_action :remove_browser_caching
 
   def team
+    @response = CompanyApi::Request::Token.new(
+        CompanyApi::Response::Formatter::Token,
+        request.cookies,
+        {"User-Agent" => http_user_agent}
+    ).fetch_token_details()
 
+    unless @response.success?
+      return handle_temporary_redirects(@response)
+    end
+
+    @presenter_obj = ::WebPresenter::TokenPresenter.new(@response, params)
+
+    #TODO: Change this temp code
+    render 'team'
   end
 
 
