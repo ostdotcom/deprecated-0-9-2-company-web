49b78816a3e2d40d41e8574d5568a1853bacda09
Shraddha <shraddha.patil@ost.com>
2019-01-16 16:22:55 +0530

    token setup progress bar api  integration

--- app/assets/javascripts/web/economy/token-setup.js
+++ app/assets/javascripts/web/economy/token-setup.js
@@ -4,21 +4,62 @@
   var ost  = ns("ost");
   var token_setup = ns("ost.token_setup");
 
-  var oThis   = token_setup = {
-  init : function () {
-    oThis.setTooltipPosition();
-    setInterval(function () {
+  var oThis   = ost.token_setup = {
+
+    intervalId: null,
+
+    init : function (config) {
+      $.extend(oThis, config);
       oThis.setTooltipPosition();
-    },10);
+      oThis.getDeployStatus();
+      oThis.intervalId = setInterval(function () {
+        oThis.getDeployStatus();
+      }, 2000);
+    },
+
+    getDeployStatus : function() {
+       $.ajax({
+         url: oThis.getDeployStatusEndPoint,
+         success:function (response) {
+           if(response && response.success){
+             oThis.updateProgessBar(response.data.workflow_current_step);
+           }
+           else{
+             console.log("api call was successful but returned false");
+           }
+         },
+         error: function () {
+           console.log("the api call was not successful");
+         }
+       })
+    },
+
+    setTooltipPosition: function(percent_completion) {
+
+        var tooltipOffsetLeft = 5;
+        var arrowHalfLength = 6;
+        var tooltipWidth = $(".tooltip").width();
+
+        if(typeof percent_completion === 'undefined') percent_completion = 0;
+
+        $(".tooltip-text").text(percent_completion +"%");
+
+        $("div.tooltip").css({
+          left: Math.max(0, percent_completion-tooltipOffsetLeft)+'%'
+        });
+        $(".arrow").css({
+          left: tooltipWidth / 2 - arrowHalfLength
+        });
+        if(percent_completion === 100){
+          oThis.intervalId.clearInterval();
+        }
+    },
+    updateProgessBar: function (currentStatus) {
+        $(".progress-bar")[0].style.width = currentStatus.percent_completion+'%';
+        $("#progressStep").html(currentStatus.display_text);
+        oThis.setTooltipPosition(currentStatus.percent_completion);
     }
-  ,setTooltipPosition: function() {
-      var width = document.getElementsByClassName("progress-bar")[0].offsetWidth;
-      $("div.tooltip").css({left:width-20+"px" });
-  },
   };
 
-  $(document).ready(function () {
-    oThis.init();
-  });
 
 })(window, jQuery);
